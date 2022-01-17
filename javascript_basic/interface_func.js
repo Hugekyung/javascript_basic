2a44cad98eec58cf6e443c32f0f8df9d46bc06dd
haechan <kiki9510@gmail.com>
2022-01-17 19:08:43 +0900

    modified some code

--- javascript_basic/interface_func.js
+++ javascript_basic/interface_func.js
@@ -8,10 +8,10 @@ alert("hello")
 // default: 입력 필드의 초기값, []는 필수가 아닌 선택값이라는 것을 의미한다
 result = prompt(title, [default]);
 
-// 사용자가 원하는 값을 입력한 뒤 확인을 누를 수 있다
+// 사용자가 원하는 값을 입력한 뒤 확인을 누를 수 있다, 취소 시 null 반환
 let age = prompt('나이를 입력해주세요.', 100);
 alert('당신의 나이는 ${age}살 입니다.');
 
-// confirm: 질문에 대한 확인과 취소가 있는 모달창
+// confirm: 질문에 대한 확인과 취소가 있는 모달창, true 나 false를 반환
 let isBoss = confirm('당신이 보스인가요');
 alert(isBoss);
