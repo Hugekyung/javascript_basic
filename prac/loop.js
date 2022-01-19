987c809dacee844d4013996f0e719ac2d5b3ea1f
haechan <kiki9510@gmail.com>
2022-01-19 16:44:24 +0900

    Added loop.js

---
+++ prac/loop.js
@@ -0,0 +1,10 @@
+// 반복문
+// 0에서 1까지 1씩 증가하며 값 출력
+for(let i=0 ; i<2 ; i=i+1){
+    console.log(i);
+}
+
+let data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for(let i=0; i<data.length; i++) {
+    console.log(data[i]);
+}
---
+++ prac/property.js
@@ -0,0 +1,12 @@
+// property, method
+str1.length;
+
+str1.charAt(0); // 인덱스로 문자열 접근
+
+str1.split(' ');
+
+str1.push("딸기"); // 배열의 뒤에 데이터 삽입
+str1.unshift("레몬"); // 배열의 앞에 데이터 삽입
+
+fruit.pop(); // 배열 뒤의 데이터 제거
+fruit.shift(); // 배열 앞의 데이터 제거
